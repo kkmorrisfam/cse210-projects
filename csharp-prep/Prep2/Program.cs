6fb6e336708ad3f0bb8f78054dc96fd222e41cf5
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-09-14 13:14:38 -0700

    Added assignments Prep1 and Prep2: working.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?  ");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+        string letterGrade;
+        int remainder = gradePercent % 10;  //get the remainder to determine sign
+        string sign = "";
+
+        if (gradePercent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letterGrade = "B";
+        }
+       else if (gradePercent >=70)
+        {
+            letterGrade = "C";
+        }
+       else if (gradePercent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+        //what's your sign
+        if (remainder >=7)
+        {
+            sign = "+";    
+        }
+        else if (remainder <= 3)
+        {
+            sign = "-";
+        }
+        //negate sign if A+ or F range
+        if (gradePercent >96 || gradePercent < 60)
+        {
+            sign = "";
+        } 
+        
+        Console.WriteLine ($"Your grade is {letterGrade}{sign}");
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Awesome! You passed the class.");
+
+        }
+        else
+        {
+            Console.WriteLine("Awe man you didn't pass the class! Better luck next time.");
+        }
     }
 }
