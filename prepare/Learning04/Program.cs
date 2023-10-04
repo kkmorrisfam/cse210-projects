cf2f3f664fd1b19066434f06958116bd09ef18cf
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-10-03 18:07:09 -0700

    add missing writeline

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -11,12 +11,13 @@ class Program
 
         MathAssignment myMathAssignment = new MathAssignment("Roberto Rodriguez", "Fraction", "Section 7.3", "Problems 8-19");
 
-        Console.WriteLine(myMathAssignment.GetSummary());
+        Console.WriteLine(myMathAssignment.GetSummary());   //can call method defined in base class
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
         Console.WriteLine();
-        
+
         WritingAssignment myWritingAssignment = new WritingAssignment("Mary Walters", "Eureopean History", "The Causes of World War II");
 
-        Console.WriteLine(myWritingAssignment.GetSummary());
+        Console.WriteLine(myWritingAssignment.GetSummary());  //method defined in base class
         Console.WriteLine(myWritingAssignment.GetWritingInformation());
     }
 }
