e7597caa8b147f4bb21329539164e1bbc62b7e22
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-10-03 18:04:08 -0700

    Practice inheritance

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Assignment
+{
+    protected string _studentName;
+    private string _topic;
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new Assignment("Mary Walters", "European History");
+        
+        Console.WriteLine(myAssignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment myMathAssignment = new MathAssignment("Roberto Rodriguez", "Fraction", "Section 7.3", "Problems 8-19");
+
+        Console.WriteLine(myMathAssignment.GetSummary());
+        Console.WriteLine();
+        
+        WritingAssignment myWritingAssignment = new WritingAssignment("Mary Walters", "Eureopean History", "The Causes of World War II");
+
+        Console.WriteLine(myWritingAssignment.GetSummary());
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
     }
 }
