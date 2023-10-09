81ff5a14d8f2370550c0d7dc3c002e18bfb11b30
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-10-09 16:01:45 -0700

    Added Rectangle and Square shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square mySquare = new Square("Blue", 4);
+        double area = mySquare.GetArea();            
+        Console.WriteLine($"Square Area: {area}");
+        Console.WriteLine($"The color is: {mySquare.GetColor()}");
+
+        Rectangle myRectangle = new Rectangle("Red", 6, 8.5);
+        Console.WriteLine($"Rectangle area: {myRectangle.GetArea()}");
+        Console.WriteLine($"Rectangle color: {myRectangle.GetColor()}");
     }
 }
