0cb92b1604057403821af4ff2200bb8212345196
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-10-24 11:22:27 -0700

    changed comment

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -3,6 +3,8 @@ public class Comment
     public string _name;
     public string _comment;
 
+    //empty constructor. With this class, I just set the variables directly in the main program. 
+    //I probably would make these private in the future and create getters and setters.
     public Comment()
     {
 
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -5,6 +5,9 @@ public class Video
     public int _length;  //seconds
     public List<Comment> _comments;
 
+    //constructor.  I set the other variables directly in the main program to show that I can
+    //with this type of project.  I would probably make them private in the future and create getters and setters
+    //or a constructor that set these values.
     public Video()
     {
         _comments = new List<Comment>();
